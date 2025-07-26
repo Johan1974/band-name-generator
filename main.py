431347c0e12f8a4aec2dc0de5a7483042e174ee7
Johan <johanlijffijt@gmail.com>
2025-07-26 23:06:51 +0000

    Initial commit: Bank Name Genrator

---
+++ main.py
@@ -0,0 +1,12 @@
+# Band Name Generator
+
+print("Welcome to the Band Name Generator")
+
+# Ask for the city the user grew up in
+city = input("What is the name of the city you grew up in?\n")
+
+# Ask for the user's pet name
+pet = input("What's your pet's name?\n")
+
+# Combine the names to generate a band name
+print(f"Your band name could be {city} {pet}")
